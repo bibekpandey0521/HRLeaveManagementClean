b3b4560941a285a585df724fbcca87ea572ba647
bibekpandey0521 <bibekpandey276@gmail.com>
2025-12-18 20:58:44 +0545

    feature-configuration-persistence

--- HRLeaveManagementApplication/Features/LeaveType/Commands/CommandLeaveType/CreateLeaveType/CreateLeaveTypeCommandValidator.cs
+++ HRLeaveManagementApplication/Features/LeaveType/Commands/CommandLeaveType/CreateLeaveType/CreateLeaveTypeCommandValidator.cs
@@ -13,13 +13,13 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CommandLeav
         public CreateLeaveTypeCommandValidator(ILeaveTypeRepository leaveTypeRepository)
         {
             RuleFor(p => p.Name)
-                .NotEmpty().WithMessage("{Property Name} is required")
+                .NotEmpty().WithMessage("{PropertyName} is required")
                 .NotNull()
-                .MaximumLength(70).WithMessage("{PropertyName} must be fewer than 70 chararacters");
+                .MaximumLength(70).WithMessage("{PropertyName} must be fewer than 70 characters");
 
             RuleFor(p => p.DefaultDays)
-                .GreaterThan(100).WithMessage("{PropertyName} cannot excedd 100")
-                .LessThan(1).WithMessage("{PropertyName} cannot be less than 1");
+                .LessThan(100).WithMessage("{PropertyName} cannot exceed 100")
+                .GreaterThan(1).WithMessage("{PropertyName} cannot be less than 1");
             RuleFor(q => q)
                 .MustAsync(LeaveTypeNameUnique)
                 .WithMessage("Leave Type already exists");
