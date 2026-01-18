0c9f1bdedd1898008cfc5beea48f952ffb3f8589
bibekpandey0521 <bibekpandey276@gmail.com>
2026-01-18 21:36:52 +0545

    feat(blazor-ui): add Leave Types UI with navigation and placeholders

---
+++ HR.LeaveManagement.BlazorUI/Pages/LeaveTypes/Index.razor.cs
@@ -0,0 +1,51 @@
+using HR.LeaveManagement.BlazorUI.Contracts;
+using HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
+using Microsoft.AspNetCore.Components;
+
+namespace HR.LeaveManagement.BlazorUI.Pages.LeaveTypes
+{
+    public partial class Index
+    {
+        [Inject]
+        NavigationManager NavigationManager { get; set; }
+        [Inject]
+        public ILeaveTypeService LeaveTypeService { get; set; }
+
+        public string Message { get; private set; }
+
+        public List<LeaveTypeVM> LeaveTypes { get; private set; }
+        protected void CreateLeaveType()
+        {
+            NavigationManager.NavigateTo("/leavetypes/create");
+        }
+        protected void AllocateLeaveType(int id)
+        {
+            // Leave Allocation Service here
+        }
+
+        protected void EditLeaveType(int id)
+        {
+            NavigationManager.NavigateTo($"/leaveTypes/edit/{id}");
+        }
+        protected void DetailsLeaveType(int id)
+        {
+            NavigationManager.NavigateTo($"/leaveTypes/details/{id}");
+        }
+        protected async Task DeleteLeaveType(int id)
+        {
+            var response = await LeaveTypeService.DeleteLeaveType(id);
+            if (response.Success)
+            {
+                StateHasChanged();
+            }
+            else
+            {
+                Message = response.Message;
+            }
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            LeaveTypes = await LeaveTypeService.GetLeaveTypes();
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
-![Image](./docs/img/Screenshot%20(187).png)
-![Image](./docs/img/Screenshot%20(188).png)
+![Image](./docs/img/Screenshot%20(189).png)
+![Image](./docs/img/Screenshot%20(193).png)
+![Image](./docs/img/Screenshot%20(194).png)
+![Image](./docs/img/Screenshot%20(195).png)
+![Image](./docs/img/Screenshot%20(196).png)
 
 
 
